f1154a6dff1f2f0e07923996f60d823ea7d6382c
768100410 <768100410@qq.com>
2018-05-04 18:27:28 +0800

    zqd

---
+++ app/Http/Controllers/Back/BackNetController.php
@@ -0,0 +1,11 @@
+<?php 
+namespace App\Http\Controllers\Back;
+use App\Http\Controllers\Back\CommonControllers;
+use App\Back\BackBookModel;
+class BackNetController extends CommonController{
+	
+	public function index(){
+		
+	}
+	
+}
